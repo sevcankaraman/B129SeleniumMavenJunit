b48c39dabad24b08216808cb864059b1486c2276
sevcan karaman <sevcankaraman@users.noreply.github.com>
2023-04-03 13:02:09 +0300

    day11selenium

---
+++ src/test/java/day10_Alert_Iframe/Task_Odev.java
@@ -0,0 +1,13 @@
+package day10_Alert_Iframe;
+
+public class Task_Odev {
+    //● https://the-internet.herokuapp.com/iframe adresine
+//gidin.
+//● Bir metod olusturun: iframeTest
+//○ “An IFrame containing….” textinin erisilebilir oldugunu test edin ve konsolda yazdirin.
+//○Text Box’a “Merhaba Dunya!” yazin.
+//○TextBox’in altinda bulunan “Elemental Selenium” linkini textinin gorunur
+//oldugunu dogrulayin ve konsolda yazdirin
+
+
+}

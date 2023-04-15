5426c0813894617be5b6e977b93b8b7c2becfebe
sevcan karaman <sevcankaraman@users.noreply.github.com>
2023-04-16 01:48:40 +0300

    selenium day 17

---
+++ src/test/java/day16_Exceptions/Home_Work.java
@@ -0,0 +1,28 @@
+package day16_Exceptions;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class Home_Work extends TestBase {
+    @Test
+    public void StaleElementReferanceExceptionTest5() {
+        /*
+        NOT:StaleElementReferanceException hatası almamız gerekiyor
+        //Amazon sayfasına gidelim
+        //iphone aratalım
+        //Çıkan sonuclardan ilk 5'ine tıklayıp sayfa başlıklarını alalım konsola yazdıralım
+        //Sayfayı kapatalım
+         */
+    }
+
+    @Test
+    public void StaleElementReferanceExceptionTest6() {
+        /*
+        NOT:Exception almadan doğru şekilde yapalım
+        //Amazon sayfasına gidelim
+        //iphone aratalım
+        //Çıkan sonuclardan ilk 5'ine tıklayıp sayfa başlıklarını alalım konsola yazdıralım
+        //Sayfayı kapatalım
+         */
+    }
+}
